4022cc9587f0aa10a9f32047849d25785c5e10b8
nbonato <akatux@gmail.com>
2021-12-04 02:19:40 +0100

    Adding second problem for day 1

--- day1.py
+++ day1.py
@@ -1,3 +1,4 @@
+#first problem
 f = open("input.txt")
 base = 0
 count = -1
@@ -6,4 +7,23 @@ for line in f:
     if line > base:
         count += 1
     base = line  
-print(count)
+print("Solution to the first problem: ", count)
+
+#second problem
+f = open("input.txt")
+
+array = []
+tracker = 0
+sumUp = 0
+for line in f:
+    array.append(int(line.rstrip()))
+
+base = 0
+count = -1
+for i in range(0, len(array)-2):
+    sumUp = sum(array[i:i+3])
+    if sumUp > base:
+        count += 1
+    base = sumUp  
+
+print("Solution to the second problem: ", count)
